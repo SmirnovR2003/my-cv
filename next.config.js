182e1fc0302be7f47c6b71f895e3bef32b5be602
m.smm2003mail.ru@gmail.com <m.smm2003mail.ru@gmail.com>
2025-02-17 19:32:15 +0300

    s

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 // next.config.js
 module.exports = {
     reactStrictMode: true,
-    assetPrefix: '', 
-    basePath: '',     
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/my-cv' : '', // Замените 'my-cv' на имя вашего репозитория
+    basePath: process.env.NODE_ENV === 'production' ? '/my-cv' : '',     // Замените 'my-cv' на имя вашего репозитория
     trailingSlash: true, 
     images: {
       unoptimized: true, 
     },
-}
+  }
